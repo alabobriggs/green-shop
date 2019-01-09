cc9650fa24facdd854d1dd31530442681210b17e
alabobriggs <alabobriggs@gmail.com>
2019-01-09 11:35:24 +0100

    add path til

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(adminRoutes)
 app.use(shopRoutes)
 
 app.use((req,res,next) => {
-    res.status(404).res.sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000, ()=> {
