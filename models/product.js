6222242a3ae18a44119c64e72da31c3e9ec2bb5b
alabobriggs <alabobriggs@gmail.com>
2019-01-09 23:14:03 +0100

    update product model

--- models/product.js
+++ models/product.js
@@ -1,18 +1,39 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+
+const p = path.join(path.dirname(
+    process.mainModule.filename),
+    'data',
+    'products.json'
+)
+
+const getProductFromFile = (callBack) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            // this callback is sued to inform the parent function when it is done reading the file
+            callBack([])
+        } else {
+            callBack(JSON.parse(fileContent))
+        }
+    })
+}
 
 module.exports = class Product {
+    // every function created from this will have title attached to it
     constructor(title){
         this.title = title
     }
 
     save(){
-        // *this pushes this which is the constructor funtionwithe all its constructors into the product array
-        products.push(this)
-        console.log(this.title)
-        console.log(products)
+        getProductFromFile(products => {
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            })
+        })  
     }
 
-    static fetchAll(){
-        return products
+    static fetchAll(callBack){
+        getProductFromFile(callBack)
     }
 }
