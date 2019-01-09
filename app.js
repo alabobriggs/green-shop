fa2f903f642077a98b02461424d735eaf43d00de
alabobriggs <alabobriggs@gmail.com>
2019-01-09 13:07:36 +0100

    install view engines

--- app.js
+++ app.js
@@ -7,18 +7,24 @@ const shopRoutes = require('./routes/shop')
 
 const app = express()
 
+// enable template engine
+app.set('view engine', 'pug')
+
 // this is used to serve static files to the server
 app.use(express.static(path.join(__dirname, 'public')))
 
+
+// enable body parser
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 
-// app.use('/admin',adminRoutes)
+// routes
 app.use('/admin',adminData.routes)
-
 app.use(shopRoutes)
 
+
+// handle 404 errors
 app.use((req,res,next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
