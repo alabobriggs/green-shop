0bf64cf9b69087804f923e4231b81f3c614bdf10
alabobriggs <alabobriggs@gmail.com>
2019-01-10 11:08:34 +0100

    add project detail page and post t cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,9 +13,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId
   Product.findById(prodId, product => {
-    console.log(product)
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    })
   })
-  res.redirect('/')
 }
 
 exports.getIndex = (req, res, next) => {
@@ -34,6 +37,12 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId
+  console.log(prodId)
+  res.redirect('/cart')
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
