cafefc2fedfd7b7a4a777db8e8ffdd4dfb0f3729
alabobriggs <alabobriggs@gmail.com>
2019-01-11 12:00:55 +0100

    remove console.log

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,7 +17,6 @@ exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
     .then(([product]) => {
-      console.log(product)
       res.render('shop/product-detail', {
         product: product[0],
         pageTitle: product.title,
