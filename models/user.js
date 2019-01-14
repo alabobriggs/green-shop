42aa6a8c5a5c5d5c04113e1e358d5c43318dca78
alabobriggs <alabobriggs@gmail.com>
2019-01-14 12:05:50 +0100

    implement get cart

--- models/user.js
+++ models/user.js
@@ -4,27 +4,27 @@ const getDb = require('../util/database').getDb
 const ObjectId = mongodb.ObjectId
 
 class User {
-  constructor(username, email, cart, id){
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
     this.cart = cart,
-    this._id = id
+      this._id = id
   }
 
-  save(){
+  save() {
     const db = getDb()
     return db.collection('users').insertOne(this)
   }
 
-  addToCart(product){
+  addToCart(product) {
     const cartProductIndex = this.cart.items.findIndex(cp => {
       return cp.productId.toString() === product._id.toString()
     })
     let newQuantity = 1;
     const updatedCartItems = [...this.cart.items]
 
-    if(cartProductIndex >= 0) {
-      newQuantity = this.cart.items[cartProductIndex].quantity +1
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1
       updatedCartItems[cartProductIndex].quantity = newQuantity
     } else {
       updatedCartItems.push({
@@ -42,21 +42,45 @@ class User {
     return db
       .collection('users')
       .updateOne(
-        {_id : new ObjectId(this._id)},
-        {$set : {cart : updatedCart}}
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
       )
   }
 
-  static findById(userId){
+  getCart() {
+    const db = getDb()
+    const productIds = this.cart.items.map(i => {
+      return i.productId
+    })
+    return db
+      .collection('products')
+      .find({_id: {$in: productIds}})
+      .toArray()  
+      .then(products => {
+        // return each products gotten from the database
+        return products.map(p => {
+          // return values of every product
+          return {
+            ...p,
+            // reach cart and get the exact quatities of the items
+            quantity: this.cart.items.find(item => {
+              return item.productId.toString() === p._id.toString()
+            }).quantity
+          }
+        })
+      })
+  }
+
+  static findById(userId) {
     const db = getDb()
     return db
       .collection('users')
-      .findOne({_id : new ObjectId(userId) })
-        .then(user => {
-          console.log(user)
-          return user
-        })
-        .catch(err => console.log(err))
+      .findOne({ _id: new ObjectId(userId) })
+      .then(user => {
+        console.log(user)
+        return user
+      })
+      .catch(err => console.log(err))
   }
 
 }
