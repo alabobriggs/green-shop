e3f4944f29b1be950a082be152c0932163636052
alabobriggs <alabobriggs@gmail.com>
2019-01-14 11:33:36 +0100

    implement add-to-cart functionality

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("5c3c4c664174c2d20313645c")
     .then(user => {
-      req.user = user;
+      req.user = new User(
+                    user.name, 
+                    user.email, 
+                    user.cart, 
+                    user._id);
       next();
     })
     .catch(err => console.log(err));
