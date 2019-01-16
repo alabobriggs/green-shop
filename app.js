6a18a4e499a78fc63a6f45454f0c2a8c985039d4
alabobriggs <alabobriggs@gmail.com>
2019-01-16 11:48:28 +0100

    restructure app.js

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
+// import INSTALLED MODULES======================================
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const session = require('express-session')
-const mongoDbStore = require('connect-mongodb-session')(session)
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
+
+// import FILE MODULES===========================================
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URl = "mongodb://localhost:27017/nodecomplete"
-
-const app = express();
-const store = new mongoDbStore({
-  url: MONGODB_URl,
-  collection: 'sessions'
-})
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
+// import ROUTES================================================
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
-app.use(bodyParser.urlencoded({
-   extended: false 
-  }));
 
+// app SETUP=========================================================
+const MONGODB_URI = "mongodb://localhost:27017/nodecomplete"
+
+// express
+const app = express();
+
+// set VIEW ENGINE
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+// set up session store
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+
+// setup body-parser
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// sessions
 app.use(session({
-  secret : 'my secret key',
+  secret: 'my secret key',
   resave: false, // stop session from changing on every response
   saveUninitialized: false, // stop session from saving on every request if nothing changes
   store: store
 }))
 
+// save user to request
 app.use((req, res, next) => {
   User.findById('5c3dae943b8b220aecd18018')
     .then(user => {
@@ -46,17 +60,19 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+// ROUTES===========================================================
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
-
 app.use(errorController.get404);
 
+// connect to DATABASE ==============================================
 mongoose
-  .connect(MONGODB_URl, {
+  .connect(MONGODB_URI, {
     useNewUrlParser: true
   })
   .then(() => {
+    // create dummy USER
     User
       .findOne()
       .then(user => {
