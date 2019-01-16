ed0043f985b77fe5e9443340e680a111cb61f2d1
alabobriggs <alabobriggs@gmail.com>
2019-01-16 09:27:43 +0100

    add login view and controller

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user')
+const User = require('./models/user');
 
 const app = express();
 
@@ -13,14 +14,15 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById("5c3dae943b8b220aecd18018")
+  User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
-      req.user = user
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
@@ -28,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
@@ -39,7 +42,7 @@ mongoose
     User
       .findOne()
       .then(user => {
-        if(!user){
+        if (!user) {
           const user = new User({
             name: 'Alabo',
             email: 'alabo@test.com',
@@ -50,6 +53,6 @@ mongoose
           user.save()
         }
       })
-   app.listen(3000, () => console.log('server started at port 3000')); 
+    app.listen(3000, () => console.log('server started at port 3000'));
   })
   .catch(err => console.log(err))
